be7a932a82eb348f99afcc97266fac492e195202
Scott Richardson <scott.richardson@visionsystemsinc.com>
2017-02-06 15:13:11 -0500

    switch back to mnist

--- mnist_acgan.py
+++ mnist_acgan.py
@@ -208,17 +208,17 @@ if __name__ == '__main__':
     # Adam parameters suggested in https://arxiv.org/abs/1511.06434
     adam_lr = 0.0002
     adam_beta_1 = 0.5
-    is_pan = False
+    is_pan = True
 
     # build the discriminator
-    discriminator = build_discriminator(is_pan=is_pan, im_size=56)
+    discriminator = build_discriminator(is_pan=is_pan, im_size=28)
     discriminator.compile(
         optimizer=Adam(lr=adam_lr, beta_1=adam_beta_1),
         loss=['binary_crossentropy', 'sparse_categorical_crossentropy']
     )
 
     # build the generator
-    generator = build_generator(latent_size, is_pan=is_pan, im_size=56)
+    generator = build_generator(latent_size, is_pan=is_pan, im_size=28)
     generator.compile(optimizer=Adam(lr=adam_lr, beta_1=adam_beta_1),
                       loss='binary_crossentropy')
 
@@ -240,8 +240,8 @@ if __name__ == '__main__':
 
     # get our mnist data, and force it to be of shape (..., 3, 56, 56) with
     # range [-1, 1]
-    (X_train, y_train), (X_test, y_test) = load_data(is_pan=is_pan, nb_images_per_label=10000)
-    #(X_train, y_train), (X_test, y_test) = mnist.load_data()
+    #(X_train, y_train), (X_test, y_test) = load_data(is_pan=is_pan, nb_images_per_label=10000)
+    (X_train, y_train), (X_test, y_test) = mnist.load_data()
 
     X_train = (X_train.astype(np.float32) - 127.5) / 127.5
     if is_pan: X_train = np.expand_dims(X_train, axis=1)
