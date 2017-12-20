5767502345ee788d7b1bfc9262e443dad406ab00
Scott Richardson <scott.richardson@visionsystemsinc.com>
2017-12-20 11:05:31 -0500

    a little simpler

--- mnist_acgan.py
+++ mnist_acgan.py
@@ -415,7 +415,7 @@ if __name__ == '__main__':
             # >>> tensor = np.ones((5,2,4,3)) * (1+np.reshape(np.arange(5), (-1,1,1,1)))/5.0
             # >>> make_grid(tensor, ncols=3)
             nb_images = tensor.shape[0]
-            tensor = np.pad(tensor, pad_width=[(0,np.mod(ncols - np.mod(nb_images, ncols), ncols))]+[(0,0)]*3,
+            tensor = np.pad(tensor, pad_width=[(0,np.abs(np.mod(nb_images, -ncols)))]+[(0,0)]*3,
                     mode='constant', constant_values=0)
             def make_col(tensor):
                 return np.vstack([im for im in tensor])
